795bf60963f138ca3a6176b281a3d7a38ee55953
GustavoRodriguez160205 <gustavorodriguezdeveloper@gmail.com>
2025-02-17 22:10:06 -0300

    Mejoramos los comentarios
    mejoramos los comentarios de auth.js y de Cards

--- client/src/Components/Card.jsx
+++ client/src/Components/Card.jsx
@@ -17,9 +17,8 @@ const Card = ({
   onVisible,
   onPublic,
   disabled,
-}) => {
-  // Seteamos los datos para las tarjetas obtenidas del home
 
+}) => {
   const [isEditing, setIsEditing] = useState(false)
   const [newNombre, setNewNombre] = useState(nombre)
   const [newEmpresa, setNewEmpresa] = useState(empresa)
@@ -30,6 +29,9 @@ const Card = ({
   const [isVisible, setIsVisible] = useState(isVisibleProp)
   const [isPublic, setIsPublic] = useState(isPublicProp)
   const { isAdmin } = useAuth()
+
+
+  // Eliminamos al usuario desde el icono de card
 
   const handleDelete = async () => {
     try {
@@ -46,6 +48,8 @@ const Card = ({
         setIsEditing(true)
   }
 
+  // Editamos el usuario
+  
   const saveEdition = async () => {
     try {
         await axios.patch(`http://localhost:5500/users/edit-users/${id}`, {
@@ -111,7 +115,6 @@ const Card = ({
               onChange={(e) => setNewEmpresa(e.target.value)}
               placeholder="Empresa"
             />
-
             <input
               className="m-0 col-md-4 col-sm-6 g-3"
               type="text"
@@ -119,7 +122,6 @@ const Card = ({
               onChange={(e) => setNewDomicilio(e.target.value)}
               placeholder="Domicilio"
             />
-
             <input
               className="m-0 col-md-4 col-sm-6 g-3"
               type="text"
@@ -127,7 +129,6 @@ const Card = ({
               onChange={(e) => setNewTelefono(e.target.value)}
               placeholder="Teléfono"
             />
-
             <input
               className="m-0 col-md-4 col-sm-6 g-3"
               type="text"
@@ -135,7 +136,6 @@ const Card = ({
               onChange={(e) => setNewCorreo(e.target.value)}
               placeholder="Correo electrónico"
             />
-
             <input
               className="m-0 col-md-4 col-sm-6 g-3"
               type="text"
@@ -170,10 +170,9 @@ const Card = ({
                 className="bi bi-geo-alt me-3"
                 viewBox="0 0 16 16"
               >
-                <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10" />
-                <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
+                <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.332 3 5.5c0-2.21 1.79-4 4-4s4 1.79 4 4c0 .832-.304 2.367-.834 3.44zM8 7a3 3 0 1 0-3-3 3 3 0 0 0 3 3z" />
               </svg>
-              {domicilio.toUpperCase()}
+              {domicilio}
             </p>
             <p className="card-text m-0">
               <svg
@@ -181,10 +180,10 @@ const Card = ({
                 width="16"
                 height="16"
                 fill="currentColor"
-                className="bi bi-telephone me-3"
+                className="bi bi-phone me-3"
                 viewBox="0 0 16 16"
               >
-                <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z" />
+                <path d="M11.742 10.344a1 1 0 0 1 .199 1.098l-1 3a1 1 0 0 1-.832.618l-2.24-.56a1 1 0 0 0-.843.332l-1.193 1.667a12.068 12.068 0 0 1-5.83-5.83l1.666-1.194a1 1 0 0 0 .332-.843l-.56-2.24a1 1 0 0 1 .618-.832l3-1a1 1 0 0 1 1.098.199l2.653 3.413a1 1 0 0 1 .198 1.097l-1 3a1 1 0 0 1-.832.618l-1.43-.358z" />
               </svg>
               {telefono}
             </p>
@@ -197,166 +196,35 @@ const Card = ({
                 className="bi bi-envelope me-3"
                 viewBox="0 0 16 16"
               >
-                <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z" />
+                <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2 0v1.236l6 4.5 6-4.5V4H2zm12 0v1.236l-6 4.5-6-4.5V4h12z" />
               </svg>
               {correo}
             </p>
-            <p className="card-text m-0">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-person-bounding-box me-3"
-                viewBox="0 0 16 16"
-              >
-                <path d="M1.5 1a.5.5 0 0 0-.5.5v3a.5.5 0 0 1-1 0v-3A1.5 1.5 0 0 1 1.5 0h3a.5.5 0 0 1 0 1zM11 .5a.5.5 0 0 1 .5-.5h3A1.5 1.5 0 0 1 16 1.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 1-.5-.5M.5 11a.5.5 0 0 1 .5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 1 0 1h-3A1.5 1.5 0 0 1 0 14.5v-3a.5.5 0 0 1 .5-.5m15 0a.5.5 0 0 1 .5.5v3a1.5 1.5 0 0 1-1.5 1.5h-3a.5.5 0 0 1 0-1h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 1 .5-.5" />
-                <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm8-9a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
-              </svg>
-              {propietario.toUpperCase()}
-            </p>
-            <p className="card-text m-0"> {isPublic} </p>
-            <p className="card-text m-0"> {isVisible} </p>
+            <p className="card-text m-0">{propietario}</p>
           </div>
         )}
+        <div className="actions col d-flex flex-column justify-content-between p-0">
+          {isAdmin && !isEditing && (
+            <>
+              <button onClick={handleEdit} className="btn btn-outline-primary mt-2">
+                Editar
+              </button>
+              <button
+                onClick={handleDelete}
+                className="btn btn-outline-danger mt-2"
+              >
+                Eliminar
+              </button>
+            </>
+          )}
+          <button onClick={handleVisibility} className="btn btn-outline-info mt-2">
+            {isVisible ? "Ocultar" : "Mostrar"}
+          </button>
+          <button onClick={handlePrivacity} className="btn btn-outline-secondary mt-2">
+            {isPublic ? "Privado" : "Público"}
+          </button>
+        </div>
       </div>
-      {isEditing ? (
-        <div className="d-flex flex-column justify-content-start">
-          <button
-            onClick={saveEdition}
-            className={`btn btn-dark ms-2 mb-2 ${disabled}`}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              className="bi bi-save"
-              viewBox="0 0 16 16"
-            >
-              <path d="M2 1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H9.5a1 1 0 0 0-1 1v7.293l2.646-2.647a.5.5 0 0 1 .708.708l-3.5 3.5a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L7.5 9.293V2a2 2 0 0 1 2-2H14a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h2.5a.5.5 0 0 1 0 1z" />
-            </svg>
-          </button>
-          <button
-            onClick={cancelEdition}
-            className={`btn btn-dark ms-2 mb-2 ${disabled}`}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              className="bi bi-x-square"
-              viewBox="0 0 16 16"
-            >
-              <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
-              <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
-            </svg>
-          </button>
-        </div>
-      ) : (
-        <div className="">
-          <button
-            onClick={handleEdit}
-            className={`btn btn-dark ms-2 mb-2 ${disabled}`}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              className="bi bi-pencil-square"
-              viewBox="0 0 16 16"
-            >
-              <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-              <path
-                fillRule="evenodd"
-                d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"
-              />
-            </svg>
-          </button>
-          <button
-            onClick={handleDelete}
-            className={`btn btn-dark ms-2 mb-2 ${disabled}`}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              className="bi bi-trash"
-              viewBox="0 0 16 16"
-            >
-              <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
-              <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
-            </svg>
-          </button>
-
-          <button
-            onClick={handleVisibility}
-            className={`btn btn-dark ms-2 mb-2 ${
-              !isPublic ? "disabled" : ""
-            } ${!isAdmin ? "disabled" : ""} ${disabled}`}
-          >
-            {isVisible ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-eye"
-                viewBox="0 0 16 16"
-              >
-                <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z" />
-                <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0" />
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-eye-slash"
-                viewBox="0 0 16 16"
-              >
-                <path d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7 7 0 0 0-2.79.588l.77.771A6 6 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755q-.247.248-.517.486z" />
-                <path d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829" />
-                <path d="M3.35 5.47q-.27.24-.518.487A13 13 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7 7 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12z" />
-              </svg>
-            )}
-          </button>
-          <button
-            onClick={handlePrivacity}
-            className={`btn btn-dark ms-2 mb-2 ${
-              isAdmin ? "disabled" : ""
-            } ${disabled}`}
-          >
-            {isPublic ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-person"
-                viewBox="0 0 16 16"
-              >
-                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-person-lock"
-                viewBox="0 0 16 16"
-              >
-                <path d="M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0M8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m0 5.996V14H3s-1 0-1-1 1-4 6-4q.845.002 1.544.107a4.5 4.5 0 0 0-.803.918A11 11 0 0 0 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664zM9 13a1 1 0 0 1 1-1v-1a2 2 0 1 1 4 0v1a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1zm3-3a1 1 0 0 0-1 1v1h2v-1a1 1 0 0 0-1-1" />
-              </svg>
-            )}
-          </button>
-        </div>
-      )}
     </div>
   )
 }
--- client/src/auth/auth.js
+++ client/src/auth/auth.js
@@ -1,9 +1,11 @@
-import axios from 'axios' // Librería para hacer solicitudes HTTP.
-import { useState, useEffect } from 'react' // Hooks de React para manejar estados y efectos secundarios.
-import { useNavigate } from 'react-router-dom' // Hook para la navegación entre rutas.
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+
+// Definimos los estados del hook
 
 const useAuth = () => {
-    // Estados para guardar la información del usuario y el estado de autenticación.
     const [isauth, setIsauth] = useState(false) 
     const [nombre, setNombre] = useState("") 
     const [empresa, setEmpresa] = useState("") 
@@ -14,28 +16,27 @@ const useAuth = () => {
     const [userId, setUserid] = useState("") 
     const [isAdmin, setIsadmin] = useState(false) 
     const navigate = useNavigate() 
-
+     
+    // Verificamos si la persona inicio sesión
     useEffect(() => {
-        // Función para verificar si el usuario está autenticado.
-        const authStatus = async () => {
+        const authStatus = async () => { 
             try {
-                // Realizamos una solicitud al servidor para verificar la autenticación.
-                const resp = await axios.get('http://localhost:5500/auth/verify', { withCredentials: true })
+                // Nos permite enviar cookies para saber si el usuario está autenticado
+                const resp = await axios.get('http://localhost:5500/auth/verify', { withCredentials: true }) 
 
-                if (resp.status === 200) { 
-                    // Si el servidor responde con éxito (status 200), actualizamos los estados con los datos del usuario.
+                if (resp.status === 200) {  // Si está todo ok guarda los datos del usuario
                     setIsauth(true)
-                    setNombre(resp.data.nombre) // Nombre del usuario desde la respuesta del servidor.
-                    setEmpresa(resp.data.empresa) // Empresa del usuario.
-                    setDomicilio(resp.data.domicilio) // Domicilio del usuario.
-                    setTelefono(resp.data.telefono) // Teléfono del usuario.
-                    setPassword(resp.data.password) // Contraseña (poco recomendable guardar en el frontend).
-                    setCorreo(resp.data.correo) // Correo electrónico.
-                    setUserid(resp.data.id) // Identificador del usuario.
-                    setIsadmin(resp.data.admin) // Rol de administrador.
-                    console.log(resp.data) // Imprimimos los datos del usuario en consola para depuración.
-                } else {
-                    // Si la respuesta no es exitosa, limpiamos los estados y marcamos al usuario como no autenticado.
+                    setNombre(resp.data.nombre)
+                    setEmpresa(resp.data.empresa)
+                    setDomicilio(resp.data.domicilio)
+                    setTelefono(resp.data.telefono)
+                    setPassword(resp.data.password)
+                    setCorreo(resp.data.correo)
+                    setUserid(resp.data.id)
+                    setIsadmin(resp.data.admin)
+                    console.log(resp.data)
+
+                } else { // Borramos los datos si la condición falla
                     setIsauth(false)
                     setNombre("")
                     setEmpresa("")
@@ -46,8 +47,7 @@ const useAuth = () => {
                     setUserid("")
                     setIsadmin("")
                 }
-            } catch (error) {
-                // Si ocurre un error (por ejemplo, el servidor no responde), limpiamos los estados y marcamos al usuario como no autenticado.
+            } catch (error) { // Borra los datos si hay un error
                 console.error(error) 
                 setIsauth(false)
                 setNombre("")
@@ -61,15 +61,19 @@ const useAuth = () => {
             }
         }
 
-        authStatus() // Llamamos a la función para verificar el estado de autenticación al montar el componente.
+        authStatus() // Ejecutamos la función
 
     }, [navigate]) 
 
+///////////////////
+///////////////////
 
+    // Cierra sesión de la app haciendo uso de la petición
     const logout = async () => {
           try {
              const resp = await axios.get('http://localhost:5500/auth/logout', {withCredentials: true} )
-             if (resp.status === 200) {
+             
+             if (resp.status === 200) { // Si se cumple la condición se borran los datos
                 setIsauth(false)
                 setNombre("")
                 setEmpresa("")
@@ -79,13 +83,13 @@ const useAuth = () => {
                 setCorreo("")
                 setUserid("")
                 setIsadmin(false)
-               
-                return true
-             
-            } else {
+                return true // Si se cerro con exito la sesión
                 
+            } else {
+
                 return false
-             }
+            }
+             // Si hubo error también elimina los datos
           } catch (error) {
             console.log(error);
             setIsauth(false)
@@ -97,14 +101,10 @@ const useAuth = () => {
             setCorreo("")
             setUserid("")
             setIsadmin(false)
-            
             return false
-
           }
     }
 
-
-    // Devolvemos los datos importantes para que los componentes que usen este hook puedan acceder a ellos.
     return {
         isauth,
         nombre, 
@@ -123,10 +123,8 @@ const useAuth = () => {
         userId,
         isAdmin,
         logout
-
     }
 
 }
 
-
-export default useAuth // Exportamos el hook para usarlo en otros componentes.
+export default useAuth
